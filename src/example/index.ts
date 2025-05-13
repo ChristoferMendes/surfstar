16342dc651a49e4ae89bfda2d9d33b07c89c9a1b
ChristoferMendes <christoferluizdsm@gmail.com>
2025-05-13 10:39:17 -0300

    refactor: simplify person information structure and update import paths

--- src/example/index.ts
+++ src/example/index.ts
@@ -1,26 +1,22 @@
-import { join } from 'node:path';
-import { compileTemplate } from '..';
+import { join } from "node:path";
+import { compileTemplate } from "surfstar";
 
-const templatePath = join(import.meta.dir, 'hello-world.surf');
+const templatePath = join(import.meta.dir, "hello-world.surf");
 
-function getPersonInfo() {
-  const bornDate = new Date('1990-01-01');
-
-  return {
-    name: 'John Doe',
-    age: new Date().getFullYear() - bornDate.getFullYear(),
-    hobbies: ['surfing', 'coding', 'reading'],
-    foods: [
-      { name: 'Pizza', price: '$10' },
-      { name: 'Burger', price: '$8' },
-      { name: 'Sushi', price: '$15' }
-    ]
-  };
-}
+const person = {
+  name: "John Doe",
+  age: 22,
+  hobbies: ["surfing", "coding", "reading"],
+  foods: [
+    { name: "Pizza", price: "$10" },
+    { name: "Burger", price: "$8" },
+    { name: "Sushi", price: "$15" },
+  ],
+};
 
 const fileCompiled = await compileTemplate(templatePath, {
   dayTimeNow: new Date().toLocaleTimeString(),
-  person: getPersonInfo()
+  person,
 });
 
 console.log(fileCompiled);
